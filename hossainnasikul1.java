b171ca0827e7aba94f25f96a70d9637daac11d42
hossainnasikul <hossainnasikul504@gmail.com>
2025-10-13 02:48:17 -0700

    hossainnasikul1
    hossainnasikul1

---
+++ hossainnasikul1.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 85;
+        if (score >= 60) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
