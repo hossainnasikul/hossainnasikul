a46f6bd3d2ed265c780981677c45e0e497eeb463
hossainnasikul <hossainnasikul504@gmail.com>
2025-10-14 04:19:16 -0700

    hossainnasikul504
    hossainnasikul504

---
+++ hossainnasikul504.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num = 7;
+    if (num > 0) {
+        cout << "Positive" << endl;
+    } else {
+        cout << "Negative" << endl;
+    }
+    return 0;
+}
